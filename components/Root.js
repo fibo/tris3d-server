41dff0c3d7dfa89202f0a1235d6e844847894e90
fibo <casati_gianluca@yahoo.it>
2016-08-07 14:04:16 -0400

    added react

---
+++ components/Root.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Root = () => (<div>Hello React</div>)
+
+export default Root
--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+import 'babel-polyfill'
+
+const socket = io()
+
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
+import Chat from './containers/Chat'
+
+const store = configureStore()
+
 import Tris3dCanvas from 'tris3d-canvas'
-
 const tris3dCanvas = new Tris3dCanvas('playground')
-
 tris3dCanvas.render()
+
+render(
+  <Provider store={store}>
+    <Chat socket={socket} />
+  </Provider>,
+  document.getElementById('react-root')
+)
