652e8b47978432d68903f57da60ae91005149ff5
fibo <casati_gianluca@yahoo.it>
2016-08-11 14:30:27 +0200

    added basic tris3d-canvas example

--- client.js
+++ client.js
@@ -1,3 +1,59 @@
+import Tris3dCanvas from 'tris3d-canvas'
+const stupid = require('tris3d-ai').stupid
+
+const tris3dCanvas = new Tris3dCanvas('playground')
+
+tris3dCanvas.on('localPlayerTurnEnds', () => {
+  console.log('wait for other players choices')
+})
+
+tris3dCanvas.on('localPlayerTurnStarts', () => {
+  console.log('is my turn')
+})
+
+tris3dCanvas.on('nextPlayer', (playerIndex) => {
+  console.log('nextPlayer', playerIndex)
+
+  const isOtherPlayerTurn = (tris3dCanvas.localPlayerIndex !== playerIndex)
+
+  // Bot choices.
+  if (isOtherPlayerTurn) {
+    const nextChoice = stupid(tris3dCanvas.choosen)
+
+    // Just a little bit of random delay.
+    var delay = 710 + Math.random() * 1700
+
+    setTimeout(() => {
+      tris3dCanvas.setChoice(playerIndex, nextChoice)
+    }, delay)
+  }
+})
+
+tris3dCanvas.on('nobodyWins', () => {
+  console.log('Nobody wins :(')
+
+  setTimeout(() => {
+    tris3dCanvas.startNewMatch()
+  }, 1700)
+})
+
+tris3dCanvas.on('setChoice', (cubeIndex) => {
+  console.log('setChoice', cubeIndex)
+})
+
+tris3dCanvas.on('tris3d!', (winnerPlayerIndex, winningCombinations) => {
+  console.log('tris3d!')
+  console.log('winnerPlayerIndex', winnerPlayerIndex)
+  console.log('winningCombinations', winningCombinations)
+
+  setTimeout(() => {
+    tris3dCanvas.startNewMatch()
+  }, 7100)
+})
+
+tris3dCanvas.render()
+
+/*
 import 'babel-polyfill'
 
 const socket = io()
@@ -20,3 +76,4 @@ render(
   </Provider>,
   document.getElementById('react-root')
 )
+*/
