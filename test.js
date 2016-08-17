037b973838405e3c599947fac8799dc7b51427a9
fibo <casati_gianluca@yahoo.it>
2016-08-17 13:09:48 +0200

    sitemap and robots test

--- test.js
+++ test.js
@@ -2,17 +2,47 @@
 
 const request = require('supertest')
 const app = require('./server')
+const should = require('should')
+const read = require('read-file-utf8')
+const path = require('path')
 
-const ok = (err, res) => {
-  if (err) throw err
-}
+const pkg = require('./package.json')
 
 describe('GET /info', () => {
-  it('responds with 200', () => {
+  it('responds with 200', (done) => {
     request(app)
       .get('/info')
       .expect('Content-Type', /json/)
       .expect(200)
-      .end(ok)
+      .end((err, res) => {
+        const name = pkg.name
+        const version = pkg.version
+
+        should.deepEqual(res.body, { name, version })
+
+        done(err)
+      })
+  })
+})
+
+describe('GET /robots.txt', () => {
+  const robotsPath = path.join(__dirname, 'public', 'robots.txt')
+
+  it('responds with 200', (done) => {
+    request(app)
+      .get('/robots.txt')
+      .expect('Content-Type', 'text/plain; charset=UTF-8')
+      .expect(200, read(robotsPath), done)
+  })
+})
+
+describe('GET /sitemap.xml', () => {
+  const sitemapPath = path.join(__dirname, 'public', 'sitemap.xml')
+
+  it('responds with 200', (done) => {
+    request(app)
+      .get('/sitemap.xml')
+      .expect('Content-Type', 'application/xml')
+      .expect(200, read(sitemapPath), done)
   })
 })
