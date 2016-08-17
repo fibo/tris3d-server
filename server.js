13732a6df4b3ff7048632013b08e44d4453e8936
fibo <casati_gianluca@yahoo.it>
2016-08-17 13:16:31 +0200

    more postcss plugins

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
+app.all('/*', (req, res) => {
+  res.redirect('/')
+})
+
 // Socket.io events.
 
 io.on('connection', (socket) => {
