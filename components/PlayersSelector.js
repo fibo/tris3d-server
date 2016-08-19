ce1675469a130b621b45f559a81d4fabbe827959
fibo <casati_gianluca@yahoo.it>
2016-08-19 18:04:19 -0400

    refactored as a class with its state

--- components/PlayersSelector.js
+++ components/PlayersSelector.js
@@ -1,41 +1,65 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 
-const PlayersSelector = ({
-  disabled
-}) => (
-  <div>
-    <select
-      name='player0'
-      defaultValue='human'
-      disabled={disabled}
-    >
-      <option value='human'>Human</option>
-      <option value='bot-stupid'>Stupid bot</option>
-      <option value='bot-smart'>Smart bot</option>
-      <option value='bot-bastard'>Bastard bot</option>
-    </select>
-    <select
-      name='player1'
-      defaultValue='bot-stupid'
-      disabled={disabled}
-    >
-      <option value='human'>Human</option>
-      <option value='bot-stupid'>Stupid bot</option>
-      <option value='bot-smart'>Smart bot</option>
-      <option value='bot-bastard'>Bastard bot</option>
-    </select>
-    <select
-      name='player2'
-      defaultValue='bot-stupid'
-      disabled={disabled}
-    >
-      <option value='human'>Human</option>
-      <option value='bot-stupid'>Stupid bot</option>
-      <option value='bot-smart'>Smart bot</option>
-      <option value='bot-bastard'>Bastard bot</option>
-    </select>
-  </div>
-)
+class PlayersSelector extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      player: [
+        'human',
+        'stupid',
+        'smart'
+      ]
+    }
+  }
+
+  render () {
+    const {
+      disabled
+    } = this.props
+
+    let {
+      player
+    } = this.state
+
+    const setState = this.setState.bind(this)
+
+    const humanPlayerIndex = player.indexOf('human')
+
+    const SelectPlayer = ({ index }) => (
+      <select
+        name='player0'
+        value={player[index]}
+        disabled={disabled}
+        onChange={(e) => {
+          const choice = e.target.value
+
+          // There can be only one human.
+          if ((choice === 'human') && (humanPlayerIndex !== index)) {
+            player[humanPlayerIndex] = player[index]
+          }
+
+          player[index] = choice
+
+          setState({ player })
+        }}
+      >
+        <option value='human'>Human</option>
+        <option value='stupid'>Stupid bot</option>
+        <option value='smart'>Smart bot</option>
+        <option value='bastard'>Bastard bot</option>
+      </select>
+    )
+
+    return (
+      <div>
+        <SelectPlayer index={0} />
+        <SelectPlayer index={1} />
+        <SelectPlayer index={2} />
+      </div>
+    )
+  }
+}
 
 PlayersSelector.propTypes = {
   disabled: PropTypes.bool.isRequired
